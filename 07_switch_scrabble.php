3826e51deb3256a437a7b83d5eb2a6147b48a795
AxelD-immalle <axel.dries@immalle.be>
2017-02-23 15:44:02 +0100

    Oefeningen

--- 07_switch_scrabble.php
+++ 07_switch_scrabble.php
@@ -3,17 +3,25 @@
 $woord = "quotering";
 $punten = 0;
 
-// gebruik een switch-case-statement binnen een for-loop om
-// te itereren over een woord en de Scrabble-punten ervan te berekenen
-
-// Q 10 punten
-// U 4 punten
-// O 1 punt
-// T 2 punten
-// E 1 punt
-// R 2 punten
-// I 1 punt
-// N 1 punt
-
+for($i = 0; $i < strlen($woord); $i++) {
+    /* echo $woord[$i]; */
+    switch(strtoupper($woord[$i])) {
+        case "Q":
+            $punten += 10;
+            break;
+        case "U":
+            $punten += 4;
+            break;
+        case "T":
+        case "R":
+            $punten += 2;
+            break;
+        case "I":
+       case "N":
+        case "E":
+            $punten += 1;
+            break;
+    }
+}
 
 ?>
